d9b90d78343615fdd8fcba646fbe019f34effbd9
Yiwen Zhu <yiwzhu@seas.upenn.edu>
2022-11-28 17:14:34 -0500

    Add html converter

---
+++ src/HtmlSyntax.hs
@@ -0,0 +1,16 @@
+module HtmlSyntax where 
+
+
+import MarkdownSyntax
+
+convertStmt :: Statement -> String 
+convertStmt = undefined
+
+convertComp :: Component -> String 
+convertComp = undefined
+
+convertMarkdown :: IO ()
+convertMarkdown = undefined
+
+-- TODO: Add model-based proprety test
+
--- src/MarkdownParser.hs
+++ src/MarkdownParser.hs
@@ -8,8 +8,7 @@ import MarkdownSyntax
 {-
 
 TODO: 
-    1) Data types for HTML (DONE)
-    2) HTML Converter (implemenation left undefined) + model-based property tests
+    1) HTML Converter (implemenation left undefined) + model-based property tests
     3) MarkdownParser (implementation left undefined) + tests
     4) HUnit tests (self-construct a Markdown syntax and te)
         a) doParse filePath ~= Block [Component [Statement]]
