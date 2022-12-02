b0de09c194b0cb3b3cd1fb709ff01e5ced900fb1
Yiwen Zhu <yiwzhu@seas.upenn.edu>
2022-12-02 13:30:45 -0500

    Refine html data structure

--- src/HtmlConverter.hs
+++ src/HtmlConverter.hs
@@ -2,12 +2,21 @@ module HtmlConverter where
 
 import MarkdownSyntax
 import HtmlEditor 
+import Data.Semigroup ( Endo(Endo, appEndo) )
 import Test.HUnit (Test(..), (~?=), (~:), runTestTT)
 
-toHTML :: Markdown -> HTML
-toHTML = undefined -- use insert here 
+newtype HTMLFile a
+  = Root [SimpleHTML a] 
+  deriving (Eq, Show, Foldable)
 
-outputHTML :: HTML -> IO ()
+htmlFile2String :: HTMLFile String -> String 
+htmlFile2String (Root elems)  = 
+  "<!DOCTYPE html>\n" ++ "<html>\n" ++ concatMap html2string elems ++ "</html>"
+      
+toHTML :: Markdown -> HTMLFile String
+toHTML = undefined -- use insert here 
+ 
+outputHTML :: HTMLFile String -> IO ()
 outputHTML = undefined
 
 -- Some simple unit tests for converting markdown to html
@@ -24,12 +33,27 @@ test1 = Markdown [
     ]
   ]
 
-expected1 :: HTML 
-expected1 = Html [
-  "<h1>Test Example 1</h1>",
-  "<hr/>",
-  "<ul><li>hello, world!</li><li>unordered<ol><li>first line</li><li>second line</li></ol></li></ul>"
+expected1 :: HTMLFile String 
+expected1 = Root [
+  Element "h1" [PCDATA "Test Example 1"],
+  Element "hr" [],
+  Element "ul" [
+    Element "li" [
+      PCDATA "hello, world!"
+      ],
+    Element "li" [
+      PCDATA "unordered",
+      Element "ol" [
+        Element "li" [PCDATA "first line"],
+        Element "li" [PCDATA "second line"]
+        ]
+      ]  
   ]
+  ]
+  -- "<h1>Test Example 1</h1>",
+  -- "<hr/>",
+  -- "<ul><li>hello, world!</li> <li>unordered <ol><li>first line</li><li>second line</li></ol></li></ul>"
+
 
 tTest1 :: Test 
 tTest1 = toHTML test1 ~?= expected1
@@ -45,12 +69,24 @@ test2 = Markdown [
   ])
   ]
 
-expected2 :: HTML 
-expected2 = Html [
-  "<h1>H1 Heading</h1>",
-  "<h3>H3 Heading</h3>",
-  "<p><em>Italicized test</em><strong>Love is bold</strong><em><strong>Bold and Italic</strong></em></p>"
+expected2 :: HTMLFile String
+expected2 = 
+  Root [
+    Element "h1" [PCDATA "H1 Heading"],
+    Element "h3" [PCDATA "H3 Heading"],
+    Element "p" [
+      Element "em" [PCDATA "Italicized test"],
+      Element "strong" [PCDATA "Love is bold"],
+      Element "em" [Element "strong" [PCDATA "Bold and Italic"]]
+    ]
   ]
+  -- -- Html [
+  -- "<h1>H1 Heading</h1>",
+  -- "<h3>H3 Heading</h3>",
+  -- "<p><em>Italicized test</em><strong>Love is bold</strong><em><strong>Bold and Italic</strong></em></p>"
+  -- ]
+
+
 
 tTest2 :: Test 
 tTest2 = toHTML test2 ~?= expected2
@@ -67,11 +103,8 @@ test3 = Markdown [
   CodeBlock "getDate()"
   ]
 
-expected3 :: HTML 
-expected3 = Html [
-  "<h5>H5 Heading</h5>",
-  "<code>getDate()</code>"
-  ]
+expected3 :: HTMLFile String
+expected3 = Root [Element "h5" [PCDATA  "H5 Heading"], Element "code" [PCDATA "getDate()"]]
 
 tTest3 :: Test 
 tTest3 = toHTML test3 ~?= expected3
--- src/HtmlEditor.hs
+++ src/HtmlEditor.hs
@@ -2,46 +2,66 @@ module HtmlEditor where
 
 import Data.Foldable qualified as Foldable
 import Data.List qualified as List
+import Data.Semigroup ( Endo(Endo, appEndo) )
 import Test.HUnit (Test(..), (~?=), (~:), runTestTT)
+
+import qualified Data.Foldable as Foldable
 
 import MarkdownSyntax
 
-type CompStr = String 
+data SimpleHTML a
+  = PCDATA a
+  | HLink a (SimpleHTML a) (Maybe a) -- HLink Href ... Title 
+  | HImage a a (Maybe a) -- HImage Alt Src Title
+  | Element a [SimpleHTML a] -- Element ElementName [..]
+  deriving (Eq, Show, Foldable)
 
-data HTML
-  = E -- empty html file
-  | Html [CompStr] -- <h1>This is CIS 552</h1>
-  deriving (Eq, Show)
+-- | Convert a SimpleHTML value to a string
+html2string :: SimpleHTML String -> String
+html2string html = go html "" where
+  go :: SimpleHTML String -> String -> String
+  go (PCDATA s) = (s ++)
+  go (HImage alt src t) = (("<a><image alt=" ++ alt ++ " src=" ++ src ++ title t ++ "></a>") ++)
+  -- -- "<a><img title='image title' alt='Upenn' src='image.jpg'></a>"
+  go (HLink href body t) = (("<a href" ++ href ++ html2string body ++ "</a>") ++)
+  -- -- "<a href='https://www.seas.upenn.edu/~cis5520/22fa'>CIS 552</a>"
+  go (Element tag []) = (("<" ++ tag ++ "/>") ++ )
+  go (Element tag body) = \s ->
+      "<" ++ tag ++ ">" ++ appEndo (foldMap (Endo . go) body) ("</" ++ tag ++ ">" ++ s)
 
-empty :: HTML 
-empty = E 
+title :: Maybe String -> String 
+title Nothing = "" 
+title (Just s) = " title=" ++ s
+
+empty :: SimpleHTML String
+empty = PCDATA ""
 
 -- insert (Heading H1 (Block [Literal "CIS 552"])) E -> Heading H1 (Block [Literal "CIS 552"])
-insert :: CompStr -> HTML -> HTML
+insert :: SimpleHTML String -> SimpleHTML String -> SimpleHTML String
 insert = undefined
 
-convComp :: Component -> [CompStr] 
+convComp :: Component -> SimpleHTML String
 convComp = undefined
 
-convStmt :: Statement -> CompStr 
+convStmt :: Statement -> SimpleHTML String
 convStmt = undefined
 
--- Unit test cases
 
+-- Unit test cases
 tTestBold :: Test
 tTestBold = 
   convStmt (Bold (Block [Literal "Love is bold"])) ~?= 
-    "<strong>Love is bold</strong>"
+    Element "strong" [PCDATA "Love is bold"]
 
 tTestItalic :: Test 
 tTestItalic = 
   convStmt (Italic (Block [Literal "This is italic text"])) ~?= 
-    "<em>This is italic text</em>"
+    Element "em" [PCDATA "This is italic text"]
 
 tTestBacktick :: Test 
 tTestBacktick = 
   convStmt (Backtick "thisIsCode()") ~?= 
-    "<code>thisIsCode()</code>"
+    Element "code" [PCDATA "thisIsCode()"]
 
 tTestLink :: Test
 tTestLink = 
@@ -50,27 +70,30 @@ tTestLink =
     "https://www.seas.upenn.edu/~cis5520/22fa" 
     Nothing
     ) ~?= 
-      "<a href='https://www.seas.upenn.edu/~cis5520/22fa'>CIS 552</a>"
+      HLink "https://www.seas.upenn.edu/~cis5520/22fa" (PCDATA "CIS 552") Nothing
+      -- "<a href='https://www.seas.upenn.edu/~cis5520/22fa'>CIS 552</a>"
 
 tTestImage :: Test
 tTestImage = 
   convStmt (
     Image "Upenn" "image.jpg" (Just "image title")
     ) ~?= 
-      "<a><img title='image title' alt='Upenn' src='image.jpg'></a>"
+      HImage "Upenn" "image.jpg" (Just "image title")
+      -- "<a><img title='image title' alt='Upenn' src='image.jpg'></a>"
 
 tTestLineBreak :: Test 
-tTestLineBreak = convStmt LineBreak ~?= "<br>"
+tTestLineBreak = convStmt LineBreak ~?= Element "br" []
 
 tTestLiteral = 
   convStmt (Literal "This is just a plain text") ~?= 
-    "This is just a plain text"
+    PCDATA "This is just a plain text"
 
 tTestHeading :: Test
 tTestHeading = 
   convComp (
     Heading H1 (Block [Literal "H1 Heading", LineBreak, Literal "Continues"])
-    ) ~?= ["<h1>", "H1 Heading", "<br>", "Continues", "</h1>"]
+    ) ~?= Element "h1" [PCDATA "H1 Heading", Element "br" [], PCDATA "Continues"]
+      --["<h1>", "H1 Heading", "<br>", "Continues", "</h1>"] 
 
 tTestParagraph :: Test
 tTestParagraph = 
@@ -78,26 +101,31 @@ tTestParagraph =
     Paragraph (Block [
       Bold (Block [Literal "Hello "]), Italic (Block [Literal "World"])
       ])
-    ) ~?= [
-      "<p>", 
-        "<strong>", "Hello ", "</strong>", 
-        "<em>", "World", "</em>", 
-      "</p>"
-    ]
-
+    ) ~?=  Element "p" [Element "strong" [PCDATA "Hello "], Element "em" [PCDATA "World"]]
+      -- "<p>", 
+      --   "<strong>", "Hello ", "</strong>", 
+      --   "<em>", "World", "</em>", 
+      -- "</p>"
+    
 tTestBlockquote :: Test
 tTestBlockquote = convComp (
   Blockquote [
     Plain "I love CIS552", 
     Newline, 
     Plain "It's best course!"
-    ]) ~?= 
-    ["<blockquote>", 
-        "I love CIS552", 
-        "<br>", 
-        "It's the best course!", 
-      "</blockquote>"
+    ]) ~?= Element "blockquote" [
+      PCDATA "I love CIS552", 
+      Element "br" [], 
+      PCDATA "It's the best course!"
       ]
+  {-
+  ["<blockquote>", 
+          "I love CIS552", 
+          "<br>", 
+          "It's the best course!", 
+        "</blockquote>"
+        ]
+  -}
 
 testOrderedList :: Component 
 testOrderedList = OrderedList [
@@ -110,19 +138,30 @@ testOrderedList = OrderedList [
     ]
   ]
 
-expectedOrderedList :: [CompStr]
-expectedOrderedList = [
-  "<ol>", 
-    "<li>", "first line", "</li>",
-    "<li>", "methods: ",
-      "<ul>", 
-        "<li>", "<code>", "getDate()", "</code>", "</li>",
-        "<li>", "<code>", "getTime()", "</code>", "</li>",
-        "<li>", "<code>", "getMinutes()", "</code>", "</li>",
-      "</ul>",
-    "</li>",
-  "</ol>"
+expectedOrderedList :: SimpleHTML String
+expectedOrderedList = Element "ol" [
+  Element "li" [PCDATA "first line"], 
+  Element "li" [
+    PCDATA "methods: ", 
+    Element "ul" [
+      Element "li" [Element "code" [PCDATA "getDate()"]],
+      Element "li" [Element "code" [PCDATA "getTime()"]],
+      Element "li" [Element "code" [PCDATA "getMinutes()"]]
+      ]
+    ]
   ]
+  -- [
+  -- "<ol>", 
+  --   "<li>", "first line", "</li>",
+  --   "<li>", "methods: ",
+  --     "<ul>", 
+  --       "<li>", "<code>", "getDate()", "</code>", "</li>",
+  --       "<li>", "<code>", "getTime()", "</code>", "</li>",
+  --       "<li>", "<code>", "getMinutes()", "</code>", "</li>",
+  --     "</ul>",
+  --   "</li>",
+  -- "</ol>"
+  -- ]
 
 tTestOrderedList :: Test 
 tTestOrderedList = convComp testOrderedList ~?= expectedOrderedList
@@ -139,83 +178,90 @@ testUnorderedList = UnorderedList [
   ] 
   ]
 
-expectedUnorderedList :: [CompStr]
-expectedUnorderedList = [
+expectedUnorderedList :: SimpleHTML String
+expectedUnorderedList = Element "ul" [
+  Element "li" [Element "h4" [PCDATA "H4 Heading"], Element "p" [PCDATA "I love Haskell"]], 
+  Element "li" [Element "h5" [PCDATA "H5 Heading"], Element "p" [PCDATA "and FP in general"]]
+  ]
+{-
+  [
   "<ul>",
     "<li>", "<h4>", "H4 Heading", "</h4>", "<p>", "I love Haskell", "</p>", "</li>",
     "<li>", "<h5>", "H5 Heading", "</h5>", "<p>", "and FP in general", "</p>", "</li>",
   "</ul>"
   ]
+  -}
 
 tTestUnorderedList :: Test 
 tTestUnorderedList = convComp testUnorderedList ~?= expectedUnorderedList
 
 tTestCodeBlock :: Test
 tTestCodeBlock = 
-  convComp (CodeBlock "a + b = c") ~?= ["<code>", "a + b = c", "</code>"]
+  convComp (CodeBlock "a + b = c") ~?= 
+    Element "code" [PCDATA "a + b = c"] --["<code>", "a + b = c", "</code>"]
 
 tTestHorinzontalRule :: Test
-tTestHorinzontalRule = convComp HorizontalRule ~?= ["<hr>"]
+tTestHorinzontalRule = convComp HorizontalRule ~?= Element "hr" [] --["<hr>"]
 
 tTestPlain :: Test
 tTestPlain = 
-  convComp (Plain "This is just a plain text") ~?= ["This is just a plain text"]
+  convComp (Plain "This is just a plain text") ~?= 
+    PCDATA "This is just a plain text"
 
 -- | Delete all matched component found 
-delete :: CompStr -> HTML -> HTML
+delete :: SimpleHTML String -> SimpleHTML String -> SimpleHTML String
 delete = undefined
 
-member :: CompStr -> HTML -> Bool 
+member :: SimpleHTML String -> SimpleHTML String -> Bool 
 member = undefined
 
-elements :: HTML -> [CompStr]
-elements (Html cs) = cs
-elements _ = []
+elements :: SimpleHTML String -> [String]
+elements = Foldable.toList
+
+-- >>> elements (HLink "https://www.seas.upenn.edu/~cis5520/22fa" (PCDATA "CIS 552") (Just "this is title"))
+-- ["https://www.seas.upenn.edu/~cis5520/22fa","CIS 552","this is title"]
+
 
 -- Below is some properties we would like to check for our html editor
 
 -- Post-Condiiton Property
-prop_FindPostPresent :: CompStr -> HTML -> Bool 
+prop_FindPostPresent :: SimpleHTML String -> SimpleHTML String-> Bool 
 prop_FindPostPresent k h = member k (insert k h)
 
-prop_FindPostAbsent :: CompStr -> HTML -> Bool 
+prop_FindPostAbsent :: SimpleHTML String -> SimpleHTML String -> Bool 
 prop_FindPostAbsent k h = not (member k (delete k h))
 
 -- Metamorphic Property 
-prop_InsertEmpty :: CompStr -> Bool 
-prop_InsertEmpty k = elements (insert k empty) == [k]
+prop_InsertEmpty :: SimpleHTML String -> Bool 
+prop_InsertEmpty k = elements (insert k empty) == elements k
 
-prop_InsertInsert :: CompStr -> CompStr -> HTML -> Bool 
+prop_InsertInsert :: SimpleHTML String-> SimpleHTML String -> SimpleHTML String-> Bool 
 prop_InsertInsert x y h = insert x (insert y h) == insert y (insert x h)
 
-prop_InsertDelete :: CompStr -> CompStr -> HTML -> Bool 
+prop_InsertDelete :: SimpleHTML String -> SimpleHTML String -> SimpleHTML String -> Bool 
 prop_InsertDelete k k0 h = 
   insert k (delete k0 h) == 
     if k == k0 then insert k h else delete k0 (insert k h)
 
-prop_MemberInsert :: CompStr -> CompStr -> HTML -> Bool 
+prop_MemberInsert :: SimpleHTML String -> SimpleHTML String-> SimpleHTML String-> Bool 
 prop_MemberInsert k k0 h = 
   member k0 (insert k h) == (k == k0 || member k0 h)
 
-prop_DeleteEmpty :: CompStr -> Bool 
+prop_DeleteEmpty :: SimpleHTML String -> Bool 
 prop_DeleteEmpty k = delete k empty == empty 
 
-prop_DeleteInsert :: CompStr -> CompStr -> HTML -> Bool 
+prop_DeleteInsert :: SimpleHTML String -> SimpleHTML String -> SimpleHTML String -> Bool 
 prop_DeleteInsert k k0 h = 
   delete k (insert k0 h) == if k == k0 then 
     if member k0 h then delete k h else h
     else insert k0 (delete k h)
 
-prop_DeleteDelete :: CompStr -> CompStr -> HTML -> Bool 
+prop_DeleteDelete :: SimpleHTML String -> SimpleHTML String -> SimpleHTML String -> Bool 
 prop_DeleteDelete x y h = 
   delete x (delete y h) == delete y (delete x h)
 
 -- Model-based Property 
-prop_InsertModel :: CompStr -> HTML -> Bool 
+prop_InsertModel :: SimpleHTML String -> SimpleHTML String -> Bool 
 prop_InsertModel k h = 
-  elements (insert k h) == List.insert k (List.delete k $ elements h)
-
-prop_DeleteModel :: CompStr -> HTML -> Bool 
-prop_DeleteModel k h = 
-  elements (delete k h) == List.delete k (elements h)
+  elements (insert k h) == elements h ++ elements k
 
